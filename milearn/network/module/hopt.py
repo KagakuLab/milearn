0810cc323f1d0c91e0fb42e87b03dfec07b1715e
Dmitry Zankov <dvzankov@gmail.com>
2025-12-03 14:01:07 +0900

    default hparam grid changed

--- milearn/network/module/hopt.py
+++ milearn/network/module/hopt.py
@@ -5,26 +5,30 @@ from concurrent.futures import ThreadPoolExecutor
 import torch
 
 DEFAULT_PARAM_GRID = {
+
     # Fixed hparams
     "max_epochs": 1000,
     "early_stopping": True,
     "accelerator": "cpu",
-    # "random_seed": 42,
+    "random_seed": 42,
     "verbose": False,
-    # Architecture depth/shape
-    "hidden_layer_sizes": [(2048, 1024, 512, 256, 128, 64), (256, 128, 64), (128,)],
+
+    # Activation function
     "activation": ["relu", "leakyrelu", "gelu", "elu", "silu"],
+
     # Learning dynamics
     "learning_rate": [10e-5, 10e-4],
     "batch_size": [32, 512, 1024],
     "weight_decay": [0.0, 1e-5, 1e-4, 1e-3, 1e-2],
+
     # MIL specific
     "tau": [0.01, 0.5, 1.0],
     "instance_dropout": [0.0, 0.2, 0.4, 0.6, 0.8],
-    # Random seed
-    "random_seed": [1, 2, 3, 4, 5],
-}
 
+    # Architecture depth/shape
+    "hidden_layer_sizes": [(2048, 1024, 512, 256, 128, 64), (256, 128, 64), (128,)],
+
+}
 
 def get_optimal_torch_threads(n_jobs: int) -> int:
     """Get optimal number of torch threads for parallel jobs.
